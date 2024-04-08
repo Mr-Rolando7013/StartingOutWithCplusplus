e52f84bb3df67d70bd8dcf677505e1d41463505d
Mr-Rolando7013 <enriquezrolandolim02@outlook.com>
2024-04-08 07:30:36 -0600

    challenge4MightDeleteItLater

---
+++ Chapter4/challenge6.cpp
@@ -0,0 +1,15 @@
+/*
+
+Mass and Weight
+
+Scientists measure an object's mass in kilograms and its weight in newtons. 
+If you know the amount of mass that an object has, you can calculate its weight, 
+in newtons, with the following formula:
+
+Weight = mass X 9,8
+
+Write a program that asks the user to enter an object's mass, then calculates and displays its weight.
+If the object weighs more than 1,000 newtons, display a message indicating that it is too heavy. 
+If the object weighs less than 10 newtons, display a mes¬ sage indicating that the object is too light.
+
+*/
